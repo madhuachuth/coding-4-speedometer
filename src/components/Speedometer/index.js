37727d7c8e543e7f94f2f1c44d02ae257ec419cd
madhuachuth <madhuachuth7@gmail.com>
2023-05-05 05:45:53 +0000

    speed

--- src/components/Speedometer/index.js
+++ src/components/Speedometer/index.js
@@ -6,23 +6,20 @@ import './index.css'
 class Speedometer extends Component {
   state = {count: 0}
 
-  onIncrement = () => {
-    const count = this.state
-    if (count < 200) {
-      this.setState(prevState => ({count: prevState.count + 10})
-      )
+  onAccelerate = () => {
+    const {count} = this.state
+    if (count < 200) this.setState(preState => ({count: preState.count + 10}))
   }
 
-  onDecrement = () => {
-    const count = this.state
-    if(count > 0) {
-        this.setState(prevState => ({count:prevState.count - 10}))
-    }
+  onBrake = () => {
+    const {count} = this.state
+    if (count > 0) this.setState(preState => ({count: preState.count - 10}))
   }
 
   render() {
-      const {count} = this.state
-      return (
+    const {count} = this.state
+
+    return (
       <div className="container">
         <h1 className="heading">SPEEDOMETER</h1>
         <img
@@ -30,25 +27,19 @@ class Speedometer extends Component {
           alt="Speedometer"
           className="speed-image"
         />
-        <h1 className="description">
-          Speedometer is <span>{count}</span>mph
-        </h1>
+        <h1 className="description">Speedometer is {count} mph</h1>
         <p className="para">Min Limit is 0mph, Max Limit is 200mph</p>
         <div className="button-container">
-          <button type="button" className="button" onClick={this.onIncrement}>
+          <button type="button" className="button" onClick={this.onAccelerate}>
             Accelerate
           </button>
-          <button
-            type="button"
-            className="button-tran"
-            onClick={this.onDecrement}
-          >
+          <button type="button" className="button-tran" onClick={this.onBrake}>
             Apply Breaks
           </button>
         </div>
       </div>
     )
-  } 
+  }
 }
 
 export default Speedometer
